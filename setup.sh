b9d91cfc1dc08595fab729f162137266137d10c6
CocoNut <vladik.ef@mail.ru>
2024-09-12 20:55:15 +0300

    fixed setup.h

--- setup.sh
+++ setup.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 # find python
-name=""
-if [ ! -z "$(python --version)" ] ; then
-    name="python"
-elif [ ! -z "$(python3 --version)" ] ; then
-    name="python3"
-else
+if [ -z "$(python3 --version)" ] ; then
     echo "No python found"
     exit 1
 fi
 
-echo "Python that's detected is $($name --version)"
+echo "Python that's detected is $($python3 --version)"
 
 # create the virtual environment, activate and set up requirements
 if [ ! -d ".venv" ] ; then
-    $name -m venv .venv
+    $python3 -m venv .venv
     source ./.venv/bin/activate
     pip install -r ./requirements.txt
 else
