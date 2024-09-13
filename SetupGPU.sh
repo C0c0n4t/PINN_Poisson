5c1be3ea176413a54b27bfe7d0cb9712f944dd62
CocoNut <vladik.ef@mail.ru>
2024-09-13 22:32:47 +0300

    Modified SetupGpu, maybe works only with downloading .deb

--- SetupGPU.sh
+++ SetupGPU.sh
@@ -7,6 +7,8 @@ sudo apt install nvidia-cuda-toolkit
 
 nvcc --version
 
+# maybe not working yet, only with pacakage from 
+# https://developer.nvidia.com/cuda-12-6-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu
 sudo apt install nvidia-cudnn
 
 sudo apt install libcudnn9-cuda-12
