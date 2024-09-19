ae8f219705ef04180416db386e6c3404946603d5
CocoNut <vladik.ef@mail.ru>
2024-09-19 22:53:00 +0300

    Added methods for accuracy, maybe make a class

--- src/accuracy.py
+++ src/accuracy.py
@@ -3,6 +3,40 @@ import numpy as np
 EPSILON = 1e-10
 
 
+#TODO: maybe make a class, that gets model and real function and compare them
+
+
+def _to_percent(error_function):
+    def wrapper_to_percent(*args, **kwargs):
+        return error_function(*args, **kwargs) * 100
+    return wrapper_to_percent
+
+
+@_to_percent
+def good_perc_rel(actual: np.ndarray, predicted: np.ndarray):
+    """
+    Percent of close values, by relative distance
+    """
+    return np.sum(np.isclose(predicted, actual, rtol=0.1)) / len(total_predictions)
+
+
+@_to_percent
+def good_perc_abs(actual: np.ndarray, predicted: np.ndarray):
+    """
+    Percent of close values, by absolute distance
+    """
+    return np.sum(np.isclose(predicted, actual, atol=0.1)) / len(total_predictions)
+
+
+@_to_percent
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5570302/
+def ve_acc(actual: np.ndarray, predicted: np.ndarray):
+    """
+    Variance Explained Accuracy
+    """
+    return 1 - np.sum(np.square(actual - predicted)) / np.sum(np.square(actual - np.mean(actual)))
+
+
 def maape(actual: np.ndarray, predicted: np.ndarray):
     """
     Mean Arctangent Absolute Percentage Error
@@ -11,11 +45,8 @@ def maape(actual: np.ndarray, predicted: np.ndarray):
     return np.mean(np.arctan(np.abs((actual - predicted) / (actual + EPSILON))))
 
 
-def good_perc(actual: np.ndarray, predicted: np.ndarray):
-    # Calculate the number of correct predictions
-    correct_predictions = np.sum(np.isclose(predicted, actual, rtol=0.1))
-
-    # Calculate the total number of predictions
-    total_predictions = len(predicted)
-
-    return correct_predictions / total_predictions
+def mse(actual: np.ndarray, predicted: np.ndarray):
+    """
+    Mean Squared Error
+    """
+    return np.mean(np.square(actual - predicted))
