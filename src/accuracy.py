30d207949e7bf6afded3dc605731cee195f0dc40
CocoNut <vladik.ef@mail.ru>
2024-09-20 21:41:04 +0300

    Added accuracy class, bad. Shitty code, fix border and area,  but gpu is WOOOOW!

--- src/accuracy.py
+++ src/accuracy.py
@@ -1,52 +1,68 @@
 import numpy as np
 
-EPSILON = 1e-10
 
+class AccuracyCalc:
+    EPSILON = 1e-10
 
-#TODO: maybe make a class, that gets model and real function and compare them
+    def __init__(self, model, real_function, dataset):
+        self._model = model
+        self._real_function = real_function
+        self._dataset = dataset
+        self._area = None
 
+    @property
+    def area(self, area: np.array):  # like getter
+        return self._area
 
-def _to_percent(error_function):
-    def wrapper_to_percent(*args, **kwargs):
-        return error_function(*args, **kwargs) * 100
-    return wrapper_to_percent
+    @area.setter
+    def area(self, area: np.array):  # like setter
+        self._area = area
 
+    def _to_percent(error_function):
+        def wrapper_to_percent(*args, **kwargs):
+            return error_function(*args, **kwargs) * 100
+        return wrapper_to_percent
 
-@_to_percent
-def good_perc_rel(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Percent of close values, by relative distance
-    """
-    return np.sum(np.isclose(predicted, actual, rtol=0.1)) / len(total_predictions)
+    @_to_percent
+    def good_perc_rel(self, area: np.ndarray | None, rel_dis: float)
+        """
+        Percent of close values, by relative distance
+        """
+        if (area.isinstance(None)):
+            return np.sum(np.isclose(
+                self._model(self._area), self._real_function(self._area),
+                rtol=rel_dis)) / len(self._area)
+        else:
+            return np.sum(np.isclose(
+                self._model(area), self._real_function(area),
+                rtol=rel_dis)) / len(area)
 
+    # @_to_percent
+    # def good_perc_abs(actual: np.ndarray, predicted: np.ndarray):
+    #     """
+    #     Percent of close values, by absolute distance
+    #     """
+    #     return np.sum(np.isclose(predicted, actual, atol=0.1)) / len(total_predictions)
 
-@_to_percent
-def good_perc_abs(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Percent of close values, by absolute distance
-    """
-    return np.sum(np.isclose(predicted, actual, atol=0.1)) / len(total_predictions)
+    # @_to_percent
+    # # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5570302/
+    # def ve_acc(actual: np.ndarray, predicted: np.ndarray):
+    #     """
+    #     Variance Explained Accuracy
+    #     """
+    #     return 1 - np.sum(np.square(actual - predicted)) / np.sum(np.square(actual - np.mean(actual)))
 
+    # def maape(actual: np.ndarray, predicted: np.ndarray):
+    #     """
+    #     Mean Arctangent Absolute Percentage Error
+    #     Note: result is NOT multiplied by 100
+    #     """
+    #     return np.mean(np.arctan(np.abs((actual - predicted) / (actual + EPSILON))))
 
-@_to_percent
-# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5570302/
-def ve_acc(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Variance Explained Accuracy
-    """
-    return 1 - np.sum(np.square(actual - predicted)) / np.sum(np.square(actual - np.mean(actual)))
+    # def mse(actual: np.ndarray, predicted: np.ndarray):
+    #     """
+    #     Mean Squared Error
+    #     """
+    #     return np.mean(np.square(actual - predicted))
 
-
-def maape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Arctangent Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.mean(np.arctan(np.abs((actual - predicted) / (actual + EPSILON))))
-
-
-def mse(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Squared Error
-    """
-    return np.mean(np.square(actual - predicted))
+    # def maxe()
